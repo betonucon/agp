76ace632162fe9957f67298eb0669dc2ec841cdd
betonucon <betonucon@gmail.com>
2022-12-23 11:32:24 +0700

    pull

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,34 +1,42 @@
 <?php
-
+  
 namespace App\Models;
-
+  
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-
+use Laravel\Sanctum\HasApiTokens;
+  
 class User extends Authenticatable
 {
-    use HasFactory, Notifiable;
-
+    use HasFactory, Notifiable, HasApiTokens;
+  
     /**
      * The attributes that are mass assignable.
      *
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name',
+        'email',
+        'cust_id',
+        'phone_number',
+        'active_status',
+        'username',
+        'password',
     ];
-
+  
     /**
      * The attributes that should be hidden for arrays.
      *
      * @var array
      */
     protected $hidden = [
-        'password', 'remember_token',
+        'password',
+        'remember_token',
     ];
-
+  
     /**
      * The attributes that should be cast to native types.
      *
@@ -37,4 +45,14 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    // function role(){
+	// 	return $this->belongsTo('App\Models\Role','role_id','id');
+    // }
+    // function person(){
+	// 	return $this->belongsTo('App\Models\MContactPerson','person_id','id');
+    // }
+    // function guest(){
+	// 	return $this->belongsTo('App\Models\Guestpersone','person_id','id');
+    // }
 }
--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\Api\AuthController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -13,7 +13,9 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::post('login', [AuthController::class, 'login']);
+Route::post('cek-login', [AuthController::class, 'cek_login']);
+Route::middleware('auth:sanctum')->group( function () {
+    Route::post('logout', [AuthController::class, 'logout']);
 });
+Route::post('register', [AuthController::class, 'register']);
