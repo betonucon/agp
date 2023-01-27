fcf5e438cd2177b852249a996cb25e139aab1b60
betonucon <betonucon@gmail.com>
2023-01-27 21:51:46 +0700

    push

--- app/Http/Controllers/Api/SoController.php
+++ app/Http/Controllers/Api/SoController.php
@@ -21,9 +21,8 @@ class SoController extends BaseController
 {
     public function create(Request $request,$tahun=null)
     {
-        error_reporting(0);
         try {
-            
+            error_reporting(0);
             $auth = $request->user(); 
             $rules = [];
             $messages = [];
@@ -52,59 +51,59 @@ class SoController extends BaseController
                     }
                     return $this->sendResponseerror($error);
             }else{
-                // echo $request->KD_Barang;
-                // $barang=Viewbarang::where('KD_Barang',$request->KD_Barang)->first();
-                // $terms=Viewjadwalsales::where('NoU',$request->NoU)->first();
-                // $customer=Customer::where('KD_Customer',$terms->KD_Customer)->first();
-                // $cek=Soheader::where('KD_Customer',$terms->KD_Customer)->where('status_data',0)->count();
-                // if($cek>0){
-                //     $mst=Soheader::where('KD_Customer',$terms->KD_Customer)->where('status_data',0)->firstOrfail();
-                //     $KD_Transaksi=$mst->KD_Transaksi;
-                // }else{
-                //     $KD_Transaksi=penomoran_so();
-                // }
-                // $save=Soheader::UpdateOrcreate([
-                //     'KD_Customer'=>$terms->KD_Customer,
-                //     'tanggal'=>date('Y-m-d'),
+                
+                $barang=Viewbarang::where('KD_Barang',$request->KD_Barang)->firstOrfail();
+                $terms=Viewjadwalsales::where('NoU',$request->NoU)->first();
+                $customer=Customer::where('KD_Customer',$terms->KD_Customer)->first();
+                $cek=Soheader::where('KD_Customer',$terms->KD_Customer)->where('status_data',0)->count();
+                if($cek>0){
+                    $mst=Soheader::where('KD_Customer',$terms->KD_Customer)->where('status_data',0)->firstOrfail();
+                    $KD_Transaksi=$mst->KD_Transaksi;
+                }else{
+                    $KD_Transaksi=penomoran_so();
+                }
+                $save=Soheader::UpdateOrcreate([
+                    'KD_Customer'=>$terms->KD_Customer,
+                    'tanggal'=>date('Y-m-d'),
                     
-                //     'status_data'=>0,
-                // ],[
-                //     'KD_Salesman'=>$auth->username,
-                //     'KD_Transaksi'=>$KD_Transaksi,
-                //     'Jatuh_Tempo'=>tanggal_tempo('+'.$terms->Term),
-                //     'Term'=>$terms->Term,
-                //     'kd_divisi'=>$terms->kd_divisi,
-                //     'Customer'=>$customer->Perusahaan,
-                //     'Customer'=>$customer->Perusahaan,
-                //     'waktu'=>date('Y-m-d H:i:s'),
-                // ]);
+                    'status_data'=>0,
+                ],[
+                    'KD_Salesman'=>$auth->username,
+                    'KD_Transaksi'=>$KD_Transaksi,
+                    'Jatuh_Tempo'=>tanggal_tempo('+'.$terms->Term),
+                    'Term'=>$terms->Term,
+                    'kd_divisi'=>$terms->kd_divisi,
+                    'Customer'=>$customer->Perusahaan,
+                    'Customer'=>$customer->Perusahaan,
+                    'waktu'=>date('Y-m-d H:i:s'),
+                ]);
 
-                // if($request->qty_free==null || $request->qty_free=='0'){
-                //     $Qtyfree=0;
-                // }else{
-                //     $Qtyfree=$request->qty_free;
-                // }
-                // if($request->discon==null || $request->discon=='0'){
-                //     $discon=0;
-                // }else{
-                //     $discon=$request->discon;
-                // }
-                // $harga=($barang['harga_ke4']-$discon);
-                // $barang=Sodetail::UpdateOrcreate([
-                //     'NoU'=>$request->NoU,
-                //     'KD_Barang'=>$request->KD_Barang,
-                //     'kd_transaksi'=>$KD_Transaksi,
-                // ],[
-                //     'qty'.$barang->hargamunculsatuanke=>$request->qty,
-                //     'Disc'.$barang->hargamunculsatuanke=>$request->discon,
-                //     'Satuan'=>$request->Satuan,
-                //     'Harga_Satuan'=>$harga,
-                //     'Qty'=>$request->qty,
-                //     'Qtyfree'=>$Qtyfree,
-                //     'total'=>($harga*$request->qty),
-                // ]);
+                if($request->qty_free==null || $request->qty_free=='0'){
+                    $Qtyfree=0;
+                }else{
+                    $Qtyfree=$request->qty_free;
+                }
+                if($request->discon==null || $request->discon=='0'){
+                    $discon=0;
+                }else{
+                    $discon=$request->discon;
+                }
+                $harga=($barang['harga_ke4']-$discon);
+                $barang=Sodetail::UpdateOrcreate([
+                    'NoU'=>$request->NoU,
+                    'KD_Barang'=>$request->KD_Barang,
+                    'kd_transaksi'=>$KD_Transaksi,
+                ],[
+                    'qty'.$barang->hargamunculsatuanke=>$request->qty,
+                    'Disc'.$barang->hargamunculsatuanke=>$request->discon,
+                    'Satuan'=>$request->Satuan,
+                    'Harga_Satuan'=>$harga,
+                    'Qty'=>$request->qty,
+                    'Qtyfree'=>$Qtyfree,
+                    'total'=>($harga*$request->qty),
+                ]);
                 $success=true;
-                return $this->sendResponse($request->KD_Barang, 'success');
+                return $this->sendResponse($success, 'success');
             }
         } catch (\Throwable $th) {
             return $this->sendResponseerror($th->getMessage(), $th->getCode());
