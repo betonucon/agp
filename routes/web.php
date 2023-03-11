df21fca04b107c43857d07c20ed38793202c6f64
betonucon <betonucon@gmail.com>
2023-03-11 11:10:16 +0700

    push

--- routes/web.php
+++ routes/web.php
@@ -117,4 +117,3 @@ Route::group(['prefix' => 'user'],function(){
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
